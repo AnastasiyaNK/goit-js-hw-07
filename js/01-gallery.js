6e2e9bb38a1a5c4b6fb8193d38e81442f043504f
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2023-07-26 22:52:51 +0200

    fix js-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,28 +3,32 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector(".gallery");
 const createGalleryEl = galleryItems.map(({ preview, original, description }) =>
-    `<li class="gallery__item"><a class="gallery__link" href="${original}" ><img class="gallery__image"
- src="${preview}" data-source="${original}" alt="${description}"></a></li>`).join("");
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}" >
+    <img class="gallery__image"
+ src="${preview}" data-source="${original}" alt="${description}"></a>
+ </li>`).join("");
 
 galleryEl.insertAdjacentHTML("beforeend", createGalleryEl)
 galleryEl.addEventListener('click', handlerClick)
 
 function handlerClick(evt) {
-    evt.preventDefault()
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__item")) {
+        return;
+    };
+    // щось перестає бібліотека працювати 
+    
+
     const galleryClickImg = evt.target.dataset.source;
     const instance = basicLightbox.create(`
     <img src="${galleryClickImg}" width="800" height="600">
 `)
     instance.show();
 
-    // galleryEl.addEventListener('keydown', (evt) => {
-    //     if (evt === "Escape") {
-    //         instance.close();
-    //     }
-    // });
-    //знайшла код у колег)але не працює у мене, будь ласка підкажіть як це зробити
+
     
-}
+};
 
  
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,8 +3,11 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector(".gallery");
 const createGalleryEl = galleryItems.map(({ preview, original, description }) =>
-    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image"
- src="${preview}" alt="${description}"></a></li>`).join("");
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+ src="${preview}" alt="${description}"></a>
+ </li>`).join("");
 
 galleryEl.insertAdjacentHTML("beforeend", createGalleryEl)
 
