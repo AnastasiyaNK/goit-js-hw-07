7b2aa7f7ed29a82578e9f6e1a03f33db6a97ce2e
AnastasiyaNK <tsoka.anastasiya@gmail.com>
2023-07-26 20:18:06 +0200

    update index.html file js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryEl = document.querySelector(".gallery");
 const createGalleryEl = galleryItems.map(({ preview, original, description }) =>
-    `<li class="gallery__item"><a class="gallery__link" href="${preview}" data-source="${original}"><img class="gallery__image"
+    `<li class="gallery__item"><a class="gallery__link" href="${original}" ><img class="gallery__image"
  src="${preview}" data-source="${original}" alt="${description}"></a></li>`).join("");
 
 galleryEl.insertAdjacentHTML("beforeend", createGalleryEl)
@@ -22,7 +22,7 @@ function handlerClick(evt) {
     //         instance.close();
     //     }
     // });
-    //не розібралась з цим, будь ласка підкажіть як це зробити
+    //знайшла код у колег)але не працює у мене, будь ласка підкажіть як це зробити
     
 }
 
